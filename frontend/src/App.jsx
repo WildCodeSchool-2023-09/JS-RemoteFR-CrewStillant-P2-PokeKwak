63c0bf556689240e359fde268436f134a5b953ef
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-10-26 16:53:52 +0200

    add section filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import CardList from "./components/CardList/CardList";
 import Footer from "./components/footer/Footer";
+import Filtres from "./components/filtres/Filtres";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <CardList />
+      <div className="main">
+        <Filtres />
+        <CardList />
+      </div>
       <Footer />
     </div>
   );
