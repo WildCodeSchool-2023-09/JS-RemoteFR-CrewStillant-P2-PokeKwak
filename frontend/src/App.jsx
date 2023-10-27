df2f50baa8d0d3b218540652bb6195e883d89945
Jhanemba64 <130657727+Jhanemba64@users.noreply.github.com>
2023-10-27 17:10:46 +0200

    Passage des Filtres dans Cardlist, fix de soucis de responsivité dans le css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,13 @@ import React, { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
 import CardList from "./components/CardList/CardList";
 import Footer from "./components/footer/Footer";
-import Filtres from "./components/filtres/Filtres";
 
 function App() {
   const [basketCount, setBasketCount] = useState(0);
   return (
     <div className="App">
       <Navbar basketCount={basketCount} />
-      <div className="main">
-        <Filtres />
-        <CardList basketCount={basketCount} setBasketCount={setBasketCount} />
-      </div>
+      <CardList basketCount={basketCount} setBasketCount={setBasketCount} />
       <Footer />
     </div>
   );
