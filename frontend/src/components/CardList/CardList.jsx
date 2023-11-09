54719df98617b5587756f9037bba7d9cb20cf9cf
BAYRAM TUGBA <tugbabayram69@gmail.com>
2023-11-09 14:41:58 +0100

    conflits resolve

---
+++ frontend/src/components/CardList/CardList.jsx
@@ -0,0 +1,49 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import styles from "./CardList.module.css";
+import Card from "../singleCard/Card";
+import Filters from "../filters/Filters";
+
+function CardList({ basketCount, setBasketCount }) {
+  const [apiData, setApiData] = useState([]);
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredCards = apiData.filter((card) =>
+    card.name.toLowerCase().startsWith(searchValue.toLowerCase())
+  );
+
+  useEffect(() => {
+    fetch("https://api.pokemontcg.io/v2/cards?pageSize=50")
+      .then((res) => res.json())
+      .then((data) => setApiData(data.data));
+  }, []);
+
+  return (
+    <div className={styles.search}>
+      <Filters setSearchValue={setSearchValue} />
+      <div className={styles.CardList}>
+        {apiData.length &&
+          filteredCards.map((p) => (
+            <Card
+              key={p.id}
+              name={p.name}
+              smallImage={p.images.small}
+              id={p.id}
+              largeImage={p.images.large}
+              basketCount={basketCount}
+              setBasketCount={setBasketCount}
+              price={p.cardmarket.prices.averageSellPrice}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default CardList;
+
+CardList.propTypes = {
+  basketCount: PropTypes.number.isRequired,
+  setBasketCount: PropTypes.func.isRequired,
+};
