6a5f6e5eb0fb5435ab7c491edad470d97945af64
Jhanemba64 <130657727+Jhanemba64@users.noreply.github.com>
2023-11-11 11:29:16 +0100

    perf: loading the API is faster

--- frontend/src/components/CardList/CardList.jsx
+++ frontend/src/components/CardList/CardList.jsx
@@ -1,41 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
 import styles from "./CardList.module.css";
 import Card from "../singleCard/Card";
 import Filters from "../filters/Filters";
 
-function CardList() {
-  const [apiData, setApiData] = useState([]);
-
+function CardList({ cards }) {
   const [searchValue, setSearchValue] = useState("");
 
-  const filteredCards = apiData.filter((card) =>
+  const filteredCards = cards.data.filter((card) =>
     card.name.toLowerCase().startsWith(searchValue.toLowerCase())
   );
 
-  useEffect(() => {
-    fetch("https://api.pokemontcg.io/v2/cards?pageSize=50")
-      .then((res) => res.json())
-      .then((data) => setApiData(data.data));
-  }, []);
-
   return (
-    <div className={styles.search}>
-      <Filters setSearchValue={setSearchValue} />
-      <div className={styles.CardList}>
-        {apiData.length &&
-          filteredCards.map((p) => (
-            <Card
-              key={p.id}
-              name={p.name}
-              smallImage={p.images.small}
-              id={p.id}
-              largeImage={p.images.large}
-              price={p.cardmarket.prices.averageSellPrice}
-            />
-          ))}
+    <div className={styles.all}>
+      <div className={styles.search}>
+        <Filters setSearchValue={setSearchValue} />
+      </div>
+      <div className={styles.cardList}>
+        {filteredCards.map((p) => (
+          <Card
+            key={p.id}
+            name={p.name}
+            smallImage={p.images.small}
+            id={p.id}
+            largeImage={p.images.large}
+            price={p.cardmarket.prices.averageSellPrice}
+          />
+        ))}
       </div>
     </div>
   );
 }
+
+CardList.propTypes = {
+  cards: PropTypes.func.isRequired,
+};
 
 export default CardList;
