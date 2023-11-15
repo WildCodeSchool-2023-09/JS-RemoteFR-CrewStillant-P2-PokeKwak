c10c8526b652c76acf387489380515888a200cb3
Jhanemba64 <130657727+Jhanemba64@users.noreply.github.com>
2023-11-15 11:14:18 +0100

    feat: add Css to the HomePage

--- frontend/src/pages/homePage/HomePage.jsx
+++ frontend/src/pages/homePage/HomePage.jsx
@@ -1,36 +1,13 @@
 import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
 import HomeImage from "../../assets/Home-image.png";
 import CatchThem from "../../assets/CatchThem.png";
 import styles from "./homePage.module.css";
-import audioFile from "../../assets/audio/song.mp3";
 
 function HomePage() {
-  const [isMuted, setIsMuted] = useState(false);
-
-  useEffect(() => {
-    const audio = new Audio(audioFile);
-
-    if (!isMuted) {
-      audio.play();
-    }
-
-    return () => {
-      audio.pause();
-      audio.currentTime = 0;
-    };
-  }, [isMuted]);
-
-  const onClickMute = () => {
-    setIsMuted((prevIsMuted) => !prevIsMuted);
-  };
-
   return (
     <div className={styles.home}>
       <img src={HomeImage} alt="logo" className={styles.smallImage} />
-      <button className={styles.button} type="button" onClick={onClickMute}>
-        {isMuted ? "Sound OFF" : "Sound ON"}
-      </button>
+
       <img src={CatchThem} alt="CatchThem" className={styles.CatchThem} />
       <div className={styles.navContent}>
         <p className={styles.welcome}>
